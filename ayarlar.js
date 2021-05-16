1cacdd52505611f6ce38d28dd6ed73b3997c683d
Lawnless <69431018+Lawnless@users.noreply.github.com>
2021-05-16 11:10:27 +0300

    Create ayarlar.js

---
+++ ayarlar.js
@@ -0,0 +1,39 @@
+module.exports = {
+	// [---------------------]
+  // [     Bot Ayarları    ]
+	// [---------------------]
+	"botPrefix": ".", // Botun prefixini girin, varsayılan değer "!"dir.
+	"botToken": "VDQyNjY2njczOTcxNTg5MTIw.YJ4pbQ.KTOzl760sAlFuFtl29lnhHVagyE", // Botun tokenini girin.
+	"botSahip": "589104745898049546", // Bot sahibinin hesap idsini girin.
+	"botDurum": "Lawnless ❤️", // Durumda sunucu adından önce gelen kısımdır.
+	"botOynuyor": "PLAYING", // "PLAYING", "LISTENING", "STREAMING", "CUSTOM_STATUS" herhangi birini kullanabilirsiniz.
+	// [---------------------]
+  // [    Emoji Ayarları   ]
+	// [---------------------]
+	"emojiUnlem": "83633626984505762", // Ünlem işareti emojisinin idsini girin.
+	"emojiSoru": "836336276701671901", // Soru işareti emojisinin idsini girin.
+	"emojiOnay": "836336288473808956", // Onay emojisinin idsini girin.
+	"emojiRed": "836336288722190300", // Red emojisinin idsini girin.
+	// [---------------------]
+  // [   Sunucu Ayarları   ]
+	// [---------------------]
+	"sunucuIsim": "Lawn's Kingdom", // Sunucu ismini girin.
+	"sunucuID": "823652318536925211", // Sunucu idsini girin.
+	"sunucuSahip": "589104745898049546", // Sunucu sahibinin hesap idsini girin.
+	"sunucuTag": "Yok", // "Yok": Sunucu tagı ayarlanmamış olur ve "•" kullanılır.
+	"sunucuYasIsaret": "•", // "Yok": Sunucu yaş ayırma işareti boşluk olur.
+	"sunucuKayitKanal": "823668692730249216", // Kayıt etme kanalını bulun ve idsini girin.
+	"sunucuKurallarKanal": "823654275208314960", // Kurallar kanalını bulun ve idsini girin.
+	"sunucuGenelKanal": "823672935717929021", // Genel sohbet kanalını bulun ve idsini girin.
+	"sunucuKayitciRol": "829005229621116958", // Kayıt sorumlusu rolünün idsini girin.
+	"sunucuKayitsizRol": "829005628944810004", // Kayıtsız rolünün idsini girin.
+	"sunucuErkekRol": "823672327450787861", // Erkek rolünün idsini girin.
+	"sunucuKadinRol": "823672349568008253", // Kadın rolünün idsini girin.
+	"sunucuVIPRol": "833319057775919154", // VIP rolünün idsini girin.
+	"sunucuDGKORol": "834757441702199326", // Doğum Günü olan kişiye verilecek rolün idsini girin.
+	// [---------------------]
+  // [  Ses Sistemi Ayar   ]
+	// [---------------------]
+	"sesKanal": "823668839907983360", // Botun bağlanacağı sesli kanalın idsini girin.
+	"sesVideoURL": "https://www.youtube.com/watch?v=JAR4LHKvV5U", // Botun çalacağı şarkıyı ya da ses efektini URL şeklinde girin.
+}
